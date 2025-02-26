ddd25cb211686af74bcc1e41a046969c31aeabf5
Anh Do <112020609+tuonganhdo@users.noreply.github.com>
2025-02-26 01:04:11 -0600

    Update server.js

--- server.js
+++ server.js
@@ -37,9 +37,9 @@ app.get('/', async (req, res) => {
     } finally {
         if (conn) conn.release();
     }
-    });
+});
 
-    // Route: Add a new customer
+// Route: Add a new customer
 app.post('/add', async (req, res) => {
     const name = req.body.name;
     // Generate a random balance between 100 and 10,000 (two decimal places)
@@ -56,6 +56,58 @@ app.post('/add', async (req, res) => {
 }
 });
 
+// Route: Add a new customer
+app.get('/greeting', async (req, res) => {
+    let conn;
+    try {
+        res.render('greeting');
+    } catch (err) {
+        res.status(500).send(`Error`);
+    }
+});
+
+app.post('/register', async (req, res) => {
+    const username = req.body.username;
+    // Generate a random balance between 100 and 10,000 (two decimal places)
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        await conn.query('INSERT INTO Users(username, name) VALUES (?, ?)', [username, username]);
+        res.redirect('/');
+    } catch (err) {
+        res.status(500).send(`Error adding user: ${err}`);    
+    } finally {
+        if (conn) conn.release();
+    }
+});
+
+app.get('/list', async (req, res) => {
+    const users = req.body.users;
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const rows = await conn.query("SELECT username FROM Users");
+        res.json({ users: rows.map(row => row.username) });
+    } catch (err) {
+        res.status(500).send(`Error`);
+    } finally {
+        if (conn) conn.release();
+    }
+});
+
+app.post('/clear', async (req, res) => {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        await conn.query('DELETE FROM Users');
+        res.redirect('/');
+    } catch (err) {
+        res.status(500).send(`Error deleting: ${err}`);    
+    } finally {
+        if (conn) conn.release();
+    }
+});
+
 app.listen(port, () => {
-    console.log(`Server is running on http://<external ip>:${port}`);
+    console.log(`Server is running on http://35.221.29.66:${port}`);
 });
